7985746604ec3c04fe21d4545980d69a4bccbf8e
Melissa Amorim <melissa18.mo@gmail.com>
2021-06-07 22:34:11 -0300

    adicionando aula 04

---
+++ AULA04/index.js
@@ -0,0 +1,40 @@
+//CRIACAO SIMPLES DE VARIAVEIS
+let nome = 'Melissa';
+let idade = 26;
+let eMaiorDeIdade = true;
+
+//CRIACAO SIMPLES DE OBJETO 
+let pessoa = {
+    nome: nome,
+    idade:idade,
+    idade: eMaiorDeIdade
+}
+
+// console.log(pessoa.nome);
+
+//CRIACAO SIMPLES DE ARRAY
+
+let animais = ['cachorro','gato','peixe','coelho'];
+
+//CRIACAO DE OPERADORES ARITMETICOS 
+
+let num1 = 10;
+let num2 = 20;
+let resultadoSoma = num1 + num2;
+
+console.log('o resultado da soma é ' + resultadoSoma);
+
+// subtracao
+
+let resultadoSubtracao = num1 - num2;
+console.log('o resultado da subtração é ' + resultadoSubtracao);
+
+//CRIACAO DE CONDICIONAL
+
+var x = 10;
+var y = 8;
+if (x > y) {
+console.log('O valor da variável X é maior que a variável Y');
+} else {
+    console.log ('O valor da váriavel Y é maior que a váriavel X');
+}
